371ce3a4e11ecd33328aea0980da99aea0582e7a
Matthew Ramos <mmramos309@gmail.com>
2020-02-03 17:29:31 -0500

    Initial Commit

---
+++ Keyboard-Handling-Lab/LoginViewController.swift
@@ -0,0 +1,83 @@
+//
+//  LoginViewController.swift
+//  Keyboard-Handling-Lab
+//
+//  Created by Matthew Ramos on 2/3/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var logoTopConstraint: NSLayoutConstraint!
+    private var originalTopConstraint: NSLayoutConstraint!
+    private var keyboardIsVisable = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerForKeyboardNotifications()
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        unregisterForKeyboardNotifications()
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+    }
+    
+    private func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc
+    private func keyboardWillShow(_ notification: NSNotification) {
+        
+        guard let keyboardFrame = notification.userInfo?[ "UIKeyboardFrameBeginUserInfoKey"] as? CGRect else {
+            return
+        }
+        moveKeyboardUp(keyboardFrame.size.height)
+    }
+    
+    @objc
+    private func keyboardWillHide(_ notification: NSNotification) {
+        
+    }
+    
+    private func moveKeyboardUp(_ height: CGFloat) {
+        if keyboardIsVisable { return }
+        keyboardIsVisable = true
+        originalTopConstraint = logoTopConstraint
+        logoTopConstraint.constant -= (height * 0.80)
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    public func resetUI() {
+        keyboardIsVisable = false
+        logoTopConstraint.constant -= originalTopConstraint.constant
+    }
+}
+
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        resetUI()
+        return true
+    }
+
+}
---
+++ Keyboard-Handling-Lab/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Keyboard-Handling-Lab
+//
+//  Created by Matthew Ramos on 2/3/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logoImageView.layer.cornerRadius = logoImageView.bounds.width / 2
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        logoImageView.alpha = 1
+        
+        pulseAnimation()
+    }
+    
+    
+    private func pulseAnimation() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            self.logoImageView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        }, completion: nil)
+    }
+    
+    private func scaleAnimation() {
+        UIView.animate(withDuration: 0.3) {
+            self.logoImageView.transform = CGAffineTransform(scaleX: 3, y: 3)
+            self.logoImageView.alpha = 0.0
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        scaleAnimation()
+    }
+    
+
+
+}
+
